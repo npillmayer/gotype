64f946277d7c342d7abb769a57ebd5fee58c2fa0
Norbert Pillmayer <norbert@pillmayer.com>
2018-01-06 18:26:33 +0100

    Minor changes

--- syntax/pmmpost/variables.go
+++ syntax/pmmpost/variables.go
@@ -1,4 +1,6 @@
 /*
+----------------------------------------------------------------------
+
 BSD License
 Copyright (c) 2017, Norbert Pillmayer
 
@@ -11,7 +13,7 @@ are met:
 2. Redistributions in binary form must reproduce the above copyright
    notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
-3. Neither the name of Tom Everett nor the names of its contributors
+3. Neither the name of Norbert Pillmayer nor the names of its contributors
    may be used to endorse or promote products derived from this software
    without specific prior written permission.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -97,22 +99,6 @@ var T tracing.Trace = tracing.InterpreterTracer
 */
 
 // === Variable Type Declarations ============================================
-
-/* We will handle 5 types of variables and 2 types of suffixes.
- */
-
-/*
-const ( // variable types
-	Undefined = iota
-	pmmp.NumericType
-	pmmp.PairType
-	PathType
-	ColorType
-	PenType
-	pmmp.ComplexArray
-	pmmp.ComplexSuffix
-)
-*/
 
 // Helper: get a type as string
 func typeString(vt int) string {
