cba105ef291a9dd0f9475fe13617eacc22f2a2b6
Norbert Pillmayer <norbert@pillmayer.com>
2018-12-11 22:27:43 +0100

    Started working on HTMLbook

---
+++ gtinput/htmlbook/html_test.go
@@ -0,0 +1,50 @@
+package htmlbook
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func TestParseFile1(t *testing.T) {
+	f, err := os.Open("small.html")
+	defer f.Close()
+	if err != nil {
+		t.Errorf("Error opening HTML file: %s", err)
+	} else {
+		dom, htmlerr := ReadHTMLbook(f)
+		if htmlerr != nil {
+			t.Errorf("Error parsing HTML file: %s", htmlerr)
+		} else {
+			txt := ""
+			h1 := dom.Find("h1")
+			h1.Each(func(i int, s *goquery.Selection) {
+				txt += s.First().Text()
+			})
+			if strings.Compare(txt, "My First HeadingMy Second Heading") != 0 {
+				t.Error("Did not parse H1 elements correctly")
+			}
+		}
+	}
+}
+
+func TestParseFile2(t *testing.T) {
+	f, err := os.Open("htmlbook.html")
+	defer f.Close()
+	if err != nil {
+		t.Errorf("Error opening HTML file: %s", err)
+	} else {
+		dom, htmlerr := ReadHTMLbook(f)
+		if htmlerr != nil {
+			t.Errorf("Error parsing HTML file: %s", htmlerr)
+		} else {
+			h1 := dom.Find("h1")
+			h1.Each(func(i int, s *goquery.Selection) {
+				txt := s.First().Text()
+				t.Logf("H1 = %s", txt)
+			})
+		}
+	}
+}
---
+++ gtinput/htmlbook/htmlbook.go
@@ -0,0 +1,23 @@
+package htmlbook
+
+// github.com/andybalholm/cascadia
+// github.com/PuerkitoBio/goquery
+
+import (
+	"io"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/npillmayer/gotype/gtcore/config/tracing"
+)
+
+// We trace to the core-tracer.
+var CT tracing.Trace = tracing.CoreTracer
+
+func ReadHTMLbook(r io.Reader) (*goquery.Document, error) {
+	doc, err := goquery.NewDocumentFromReader(r)
+	//doc, err := html.Parse(r)
+	if err != nil {
+		CT.Errorf("Unable to parse HTML file: %s", err)
+	}
+	return doc, err
+}
