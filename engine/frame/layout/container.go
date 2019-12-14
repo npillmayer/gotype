ad7633ff9ef607de47b9c28d25205d20d1346ddf
Norbert Pillmayer <norbert@pillmayer.com>
2019-12-14 22:27:13 +0100

    continue implementation of render-tree

--- engine/frame/layout/container.go
+++ engine/frame/layout/container.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/npillmayer/gotype/core/config/tracing"
 	"github.com/npillmayer/gotype/engine/dom/cssom/style"
 	"github.com/npillmayer/gotype/engine/frame/box"
 	"github.com/npillmayer/gotype/engine/tree"
@@ -13,34 +12,40 @@ import (
 
 // Flags for box context and display level.
 const (
-	NoMode uint8 = iota
-	BlockMode
-	InlineMode
-	DisplayNone
+	NoMode      uint8 = iota // in most cases an error condition
+	BlockMode                // CSS block context / display level
+	InlineMode               // CSS inline context / display level
+	DisplayNone              // CSS display = none
 )
 
 // Container is a (CSS-)styled box which may contain other boxes and/or
 // containers.
 type Container struct {
-	tree.Node
-	Box                box.StyledBox
-	contextOrientation uint8      // context of children (block or inline)
-	displayLevel       uint8      // container lives in this mode (block or inline)
-	styleNode          *tree.Node // the DOM node this Container refers to
-	styleInterf        style.StyleInterf
+	tree.Node                            // a container is a node within the layout tree
+	Box                box.StyledBox     // styled box wich is layed out
+	contextOrientation uint8             // context of children (block or inline)
+	displayLevel       uint8             // container lives in this mode (block or inline)
+	styleNode          *tree.Node        // the DOM node this Container refers to
+	styleInterf        style.StyleInterf // adapter to styled tree type
 }
 
 // newContainer creates either a block-level container or an inline-level container
-func newContainer(orientation uint8, level uint8) *Container {
+func newContainer(orientation uint8, displayLevel uint8) *Container {
 	c := &Container{
 		contextOrientation: orientation,
-		displayLevel:       level,
+		displayLevel:       displayLevel,
 	}
 	c.Payload = c // always points to itself
 	return c
 }
 
 // isPrincipal is a predicate wether this is a principal box.
+//
+// Some HTML elements create a mini-hierachy of boxes for rendering. The outermost box
+// is called the principal box. It will always refer to the styled node.
+// An example would be an "li"-element: it will create two sub-boxes, one for the
+// list item marker and one for the item's text/content. Another example are anonymous
+// boxes, which will be generated for reconciling context/level-discrepancies.
 func (c *Container) isPrincipal() bool {
 	return (c.styleNode != nil)
 }
@@ -61,8 +66,8 @@ func newInlineBox(sn *tree.Node, styleInterf style.StyleInterf) *Container {
 	return c
 }
 
-// Node gets the payload of a tree node as a Container.
-// Called from clients as
+// Node retrieves the payload of a tree node as a Container.
+// Will be called from clients as
 //
 //    container := layout.Node(n)
 //
@@ -103,72 +108,6 @@ func (c *Container) Add(child *Container) *Container {
 	}
 	c.AddChild(&child.Node)
 	return c
-}
-
-func requiresAnonBox(context uint8, level uint8) bool {
-	return ((context == BlockMode && level == InlineMode) ||
-		(context == InlineMode && level == BlockMode))
-}
-
-func wrapInAnonymousBox(c *Container) *Container {
-	mode := BlockMode
-	if c.displayLevel == BlockMode {
-		mode = InlineMode
-	}
-	anon := newContainer(mode, c.contextOrientation)
-	anon.AddChild(&c.Node)
-	return anon
-}
-
-// ----------------------------------------------------------------------
-
-func boxForNode(sn *tree.Node, toStyler style.StyleInterf) *Container {
-	context := GetFormattingContextForStyledNode(sn, toStyler)
-	if context == NoMode {
-		return nil
-	}
-	var c *Container
-	if context == BlockMode {
-		c = newBlockBox(sn, toStyler)
-	} else {
-		c = newInlineBox(sn, toStyler)
-	}
-	c.displayLevel, _ = getDisplayLevelForStyledNode(sn, toStyler)
-	return c
-}
-
-// --- Default Display Properties ---------------------------------------
-
-// GetFormattingContextForStyledNode gets the formatting context for a
-// container resulting from a
-// styled node. The context denotes the orientation in which a box's content
-// is layed out. It may be either InlineMode, BlockMode or NoMode.
-func GetFormattingContextForStyledNode(sn *tree.Node, toStyler style.StyleInterf) uint8 {
-	if sn == nil {
-		return NoMode
-	}
-	styler := toStyler(sn)
-	pmap := styler.ComputedStyles()
-	if val, _ := style.GetLocalProperty(pmap, "display"); val == "none" {
-		return NoMode
-	}
-	htmlnode := styler.HtmlNode()
-	if htmlnode.Type != html.ElementNode {
-		T().Errorf("Have styled node for non-element ?!?")
-		return InlineMode
-	}
-	switch htmlnode.Data {
-	case "body", "div", "ul", "ol", "section":
-		return BlockMode
-	//case "p", "span", "it", "h1", "h2", "h3", "h4", "h5", "h6",
-	//	"h7", "b", "i", "strong":
-	//	return InlineMode
-	default:
-		return InlineMode
-	}
-	tracing.EngineTracer.Infof("unknown HTML element %s will stack children vertically",
-		htmlnode.Data)
-	return BlockMode
 }
 
 func getDisplayLevelForStyledNode(sn *tree.Node, toStyler style.StyleInterf) (uint8, string) {
--- engine/frame/layout/generate.go
+++ engine/frame/layout/generate.go
@@ -5,8 +5,10 @@ package layout
 // - which context the element should span for its children
 
 import (
+	"github.com/npillmayer/gotype/core/config/tracing"
 	"github.com/npillmayer/gotype/engine/dom/cssom/style"
 	"github.com/npillmayer/gotype/engine/tree"
+	"golang.org/x/net/html"
 )
 
 // Helper struct to pack a node of a styled tree.
@@ -67,4 +69,88 @@ func makeBoxNode(sn stylednode, styleToBox *styleToBoxAssoc) (*tree.Node, error)
 		}
 	}
 	return &box.Node, nil
+}
+
+func requiresAnonBox(context uint8, level uint8) bool {
+	return ((context == BlockMode && level == InlineMode) ||
+		(context == InlineMode && level == BlockMode))
+}
+
+func wrapInAnonymousBox(c *Container) *Container {
+	mode := BlockMode
+	if c.displayLevel == BlockMode {
+		mode = InlineMode
+	}
+	anon := newContainer(mode, c.contextOrientation)
+	anon.AddChild(&c.Node)
+	return anon
+}
+
+// ----------------------------------------------------------------------
+
+func boxForNode(sn *tree.Node, toStyler style.StyleInterf) *Container {
+	if sn == nil {
+		return nil
+	}
+	context := GetFormattingContextForStyledNode(sn, toStyler)
+	if context == DisplayNone { // should not be displayed (display = none)
+		return nil
+	}
+	var c *Container
+	if context == BlockMode {
+		c = newBlockBox(sn, toStyler)
+	} else {
+		c = newInlineBox(sn, toStyler)
+	}
+	c.displayLevel, _ = getDisplayLevelForStyledNode(sn, toStyler)
+	possiblyCreateMiniHierarchy(c)
+	return c
+}
+
+func possiblyCreateMiniHierarchy(c *Container) {
+	htmlnode := c.styleInterf(c.styleNode).HtmlNode()
+	//propertyMap := styler.ComputedStyles()
+	switch htmlnode.Data {
+	case "li":
+		markertype, _ := style.GetCascadedProperty(c.styleNode, "list-style-type", c.styleInterf)
+		if markertype != "none" {
+			markerbox := newInlineBox(nil, nil)
+			// TODO: fill box with correct marker symbol
+			c.Add(markerbox)
+		}
+	}
+}
+
+// --- Default Display Properties ---------------------------------------
+
+// GetFormattingContextForStyledNode gets the formatting context for a
+// container resulting from a
+// styled node. The context denotes the orientation in which a box's content
+// is layed out. It may be either InlineMode, BlockMode or NoMode.
+func GetFormattingContextForStyledNode(sn *tree.Node, toStyler style.StyleInterf) uint8 {
+	if sn == nil {
+		return NoMode
+	}
+	styler := toStyler(sn)
+	pmap := styler.ComputedStyles()
+	if val, _ := style.GetLocalProperty(pmap, "display"); val == "none" {
+		return DisplayNone
+	}
+	htmlnode := styler.HtmlNode()
+	if htmlnode.Type != html.ElementNode {
+		T().Errorf("Have styled node for non-element ?!?")
+		return InlineMode
+	}
+	switch htmlnode.Data {
+	case "body", "div", "ul", "ol", "section":
+		return BlockMode
+	//case "p", "span", "it", "h1", "h2", "h3", "h4", "h5", "h6",
+	//	"h7", "b", "i", "strong":
+	//	return InlineMode
+	default:
+		return InlineMode
+	}
+	tracing.EngineTracer.Infof("unknown HTML element %s will stack children vertically",
+		htmlnode.Data)
+	return BlockMode
 }
