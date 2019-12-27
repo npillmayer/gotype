49b9cdb70516980b4217fb56ea4a877dedf69277
Norbert Pillmayer <norbert@pillmayer.com>
2019-12-27 21:50:48 +0100

    Bugfix

--- engine/frame/layout/map.go
+++ engine/frame/layout/map.go
@@ -4,29 +4,34 @@ import (
 	"sync"
 
 	"github.com/npillmayer/gotype/engine/dom"
+	"golang.org/x/net/html"
 )
 
 type domToBoxAssoc struct {
 	sync.RWMutex
-	m map[*dom.W3CNode]*Container
+	m map[*html.Node]*Container
 }
 
 func newAssoc() *domToBoxAssoc {
 	return &domToBoxAssoc{
-		m: make(map[*dom.W3CNode]*Container),
+		m: make(map[*html.Node]*Container),
 	}
 }
 
 func (d2c *domToBoxAssoc) Put(domnode *dom.W3CNode, c *Container) {
 	d2c.Lock()
 	defer d2c.Unlock()
-	d2c.m[domnode] = c
+	d2c.m[domnode.HTMLNode()] = c
+	T().Infof("%s -> %s", domnode.NodeName(), ModeString(c.contextOrientation))
 }
 
 func (d2c *domToBoxAssoc) Get(domnode *dom.W3CNode) (*Container, bool) {
 	d2c.RLock()
 	defer d2c.RUnlock()
-	c, ok := d2c.m[domnode]
+	c, ok := d2c.m[domnode.HTMLNode()]
+	if ok {
+		T().Infof("%s => %s", domnode.NodeName(), ModeString(c.contextOrientation))
+	}
 	return c, ok
 }
 
