1166bb52e92480f50e8f7779156c09ea0f49b930
Norbert Pillmayer <norbert@pillmayer.com>
2020-01-07 00:28:57 +0100

    Changed signature of predicates

--- engine/dom/actions.go
+++ engine/dom/actions.go
@@ -4,7 +4,7 @@ import "github.com/npillmayer/gotype/engine/tree"
 
 // NodeIsText is a predicate to match text-nodes of a DOM.
 // It is intended to be used in a tree walker.
-var NodeIsText = func(n *tree.Node) (match *tree.Node, err error) {
+var NodeIsText = func(n *tree.Node, unused *tree.Node) (match *tree.Node, err error) {
 	domnode, err := NodeFromTreeNode(n)
 	if err != nil {
 		return nil, err
