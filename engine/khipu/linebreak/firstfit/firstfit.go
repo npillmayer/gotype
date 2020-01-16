99ba11cccece3c5f1bcdcf0e24bb85184ad675b5
Norbert Pillmayer <norbert@pillmayer.com>
2020-01-16 21:00:33 +0100

    Changes for first-fit line-breaking

--- engine/khipu/linebreak/firstfit/firstfit.go
+++ engine/khipu/linebreak/firstfit/firstfit.go
@@ -1,4 +1,37 @@
 /*
+Package firstfit implements a straightforward line-breaking algorithm
+where lines are broken at the first suitable breakpoint.
+
+Wikipedia:
+
+	1. |  SpaceLeft := LineWidth
+	2. |  for each Word in Text
+	3. |      if (Width(Word) + SpaceWidth) > SpaceLeft
+	4. |           insert line break before Word in Text
+	5. |           SpaceLeft := LineWidth - Width(Word)
+	6. |      else
+	7. |           SpaceLeft := SpaceLeft - (Width(Word) + SpaceWidth)
+
+With khipus, we have space before words, as a rule, not after them. Additionally, we
+break at penalty knots, not on whitespace. However, the implementation looks
+roughly like this:
+
+	 1. |  SpaceUsed := 0; firstInLine := true
+	 2. |  for each Knot in Khipu
+	 3. |      if Knot.Type == Word
+	 4. |          if (SpaceUsed + Width(Word)).MinWidth > LineWidth
+	 5. |             insert line break before Word in Text
+	 6. |             SpaceUsed := 0
+	 7. |             firstInLine := true
+	 8. |          else
+	 9. |             SpaceUsed := SpaceUsed + (Width(Word) + SpaceWidth)
+	10. |             firstInLine := false
+	11. |      else if not firstInLine
+	12. |          SpaceUsed := SpaceUsed + Width(Knot)
+
+What counts as a word is not so clear with international scripts. We rely on the
+khipukamayuq to insert appropriate penalties before line-breaking happens.
+
 BSD License
 
 Copyright (c) 2017–20, Norbert Pillmayer
@@ -34,80 +67,148 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
 package firstfit
 
 import (
+	"errors"
+	"fmt"
+
+	"github.com/npillmayer/gotype/core/config/gtrace"
+	"github.com/npillmayer/gotype/core/config/tracing"
 	"github.com/npillmayer/gotype/engine/khipu"
 	"github.com/npillmayer/gotype/engine/khipu/linebreak"
 )
 
-/*
-Wikipedia:
+// T traces to the core tracer.
+func T() tracing.Trace {
+	return gtrace.CoreTracer
+}
 
-	1. |  SpaceLeft := LineWidth
-	2. |  for each Word in Text
-	3. |      if (Width(Word) + SpaceWidth) > SpaceLeft
-	4. |         insert line break before Word in Text
-	5. |         SpaceLeft := LineWidth - Width(Word)
-	6. |      else
-	7. |         SpaceLeft := SpaceLeft - (Width(Word) + SpaceWidth)
-*/
+// We use a small object to manage line-breaking information for a client call.
+// We do not expose it to clients right now, but this may change as soon as we
+// make line-breaking scriptable.
+type linebreaker struct {
+	buffer    []khipu.Mark
+	knot      khipu.Knot
+	params    *linebreak.Parameters
+	cursor    linebreak.Cursor
+	parshape  linebreak.ParShape
+	eof       bool
+	pos       int
+	check     int
+	linecount int
+}
 
-// Line 3 seems to be an error: We need to just fit in the word, not word+space.
-// All depends wether the space is counted before the word or afterwards. With khipus
-// we have space before words, as a rule.
-// Therefore we implement:
-/*
-	1. |  SpaceUsed := 0; firstInLine := true
-	2. |  for each Knot in Khipu
-	2. |      if Knot.Type == Word
-	3. |         if (SpaceUsed + Width(Word)).MinW > LineWidth
-	4. |            insert line break before Word in Text
-	5. |            SpaceUsed := 0
-	3. |            firstInLine := true
-	6. |         else
-	7. |            SpaceUsed := SpaceUsed + (Width(Word) + SpaceWidth)
-	8. |            firstInLine := false
-	8. |      else if not firstInLine
-	8. |         SpaceUsed := SpaceUsed + Width(Knot)
-*/
-
-// What counts as a word is not so clear with international scripts. We rely on the
-// khipukamayuq to insert appropriate penalties before line-breaking happens.
+// newLinebreaker creates an adequately initialized first-fit linebreaker.
+// If params are not given, default parameters will be used.
+// cursor and parshape are mandatory.
+func newLinebreaker(cursor linebreak.Cursor, parshape linebreak.ParShape,
+	params *linebreak.Parameters) (*linebreaker, error) {
+	if cursor == nil || parshape == nil {
+		return nil, errors.New("cannot break a paragraph without cursor or parshape")
+	}
+	if params == nil {
+		params = linebreak.DefaultParameters
+	}
+	lb := &linebreaker{
+		cursor:   cursor,
+		parshape: parshape,
+		params:   params,
+		buffer:   make([]khipu.Mark, 0, 20), // backtracking buffer
+		pos:      -1,
+		check:    -1,
+		//segment:  &segment{},
+	}
+	return lb, nil
+}
 
 // BreakParagraph will find 'first fit' breakpoints for a paragraph of text.
 // The method is similar to the one usually used by web browsers. It simply collects
 // line material until the current line-length is exhausted, then continues on a
 // new line.
-func BreakParagraph(cursor linebreak.Cursor, parshape linebreak.Parshape,
+func BreakParagraph(cursor linebreak.Cursor, parshape linebreak.ParShape,
 	params *linebreak.Parameters) ([]khipu.Mark, error) {
 	//
-	spaceUsed := linebreak.WSS
+	lb, err := newLinebreaker(cursor, parshape, params)
+	if err != nil {
+		return nil, err
+	}
+	return lb.FindBreakpoints()
+}
+
+func (lb *linebreaker) FindBreakpoints() ([]khipu.Mark, error) {
 	firstInLine := true
-	breakpoints := make([]khipu.Mark, 0, 10)
+	breakpoints := make([]khipu.Mark, 1, 10)
+	breakpoints[0] = provisionalMark(-1)
 	lineno := 0
-	for cursor.Next() {
-		if cursor.Mark().Knot().Type() == khipu.KTPenalty { // TODO discretionaries
-			penalty, mark := penaltyAt(cursor)
-			linelen := parshape.LineWidth(lineno)
-			if spaceUsed.MinW() >= linelen {
-			}
-			if penalty < linebreak.InfinityDemerits {
-				if penalty <= params.Tolerance {
-					// immediate break
-					firstInLine = true
+	spaceUsed := &segment{}
+	knot := lb.next()
+	xxx := 0
+	last := lb.mark()
+	for knot != nil {
+		xxx++
+		if xxx > 28 {
+			//break
+		}
+		linelen := lb.parshape.LineLength(lineno)
+		gtrace.CoreTracer.Debugf("_______________ %v ___________________", knot)
+		if knot.Type() == khipu.KTPenalty { // TODO discretionaries
+			last = lb.mark()
+			penalty := lb.penalty()
+			spaceUsed.append(knot)
+			segm := spaceUsed.width(lb.params)
+			if penalty.Demerits() < linebreak.InfinityDemerits {
+				T().Debugf("penalty %v is acceptable", penalty.Demerits())
+				T().Debugf("segm=%v", segm)
+				if segm.Min > linelen { // overshoot
+					if frag := lb.backtrack(); frag != nil {
+						// start new line
+						T().Infof("backtracked to %v", lb.knot)
+						breakpoints = lb.linebreak(breakpoints)
+						//spaceUsed.reset(frag)
+						firstInLine = true
+					} else {
+						// overfull hbox
+						T().Infof("Overfull box at line %d", lb.linecount+1)
+						// start new line
+						breakpoints = lb.linebreak(breakpoints)
+						spaceUsed.reset(linebreak.WSS{})
+						firstInLine = true
+					}
+				} else if segm.Max >= linelen { // can reach EOL
+					if penalty.Demerits() <= lb.params.Tolerance {
+						if !lb.checkpoint() {
+							panic("CANNOT SET CHECKPOINT")
+						}
+					} else {
+						// save for later review, if no other feasible break is found ? How ?
+						if !lb.checkpoint() {
+							panic("CANNOT SET CHECKPOINT")
+						}
+						gtrace.CoreTracer.Infof("setting checkpoint with demerits=%v", penalty.Demerits())
+					}
 				} else {
-					// save for later review, if no other feasible break is found ? How ?
+					if !lb.checkpoint() {
+						panic("CANNOT SET CHECKPOINT")
+					}
+					gtrace.CoreTracer.Infof("setting emergency with demerits=%v", penalty.Demerits())
 				}
 			} else {
 				// no feasible break
 			}
-		} else if !firstInLine {
-			// TODO
-			// append to line
+		} else if firstInLine {
+			if !knot.IsDiscardable() { // do not add space to front of line
+				spaceUsed.append(knot)
+				firstInLine = false
+			}
+		} else { // append everything to line
+			spaceUsed.append(knot)
 		}
+		T().Debugf("segment = %v", spaceUsed)
+		knot = lb.next()
 	}
-	return nil, breakpoints
+	breakpoints = append(breakpoints, last)
+	return breakpoints, nil
 }
 
-// penaltyAt iterates over all penalties, starting at the current cursor mark, and
+// penaltyAt iterates over all penalties, starting at the current cursor check, and
 // collects penalties, searching for the most significant one.
 // Will return
 //
@@ -115,32 +216,151 @@ func BreakParagraph(cursor linebreak.Cursor, parshape linebreak.Parshape,
 //        max(p1, p2, ..., pn) otherwise
 //
 // Returns the most significant penalty. Advances the cursor over all adjacent penalties.
-// After this, the cursor mark may not reflect the position of the significant penalty.
-func penaltyAt(cursor linebreak.Cursor) (khipu.Penalty, khipu.Mark) {
-	if cursor.Knot().Type() != khipu.KTPenalty {
-		return khipu.Penalty(linebreak.InfinityDemerits), cursor.Mark()
+// After this, the cursor check may not reflect the position of the significant penalty.
+func (lb *linebreaker) penalty() khipu.Penalty {
+	if lb.eof || lb.knot.Type() != khipu.KTPenalty {
+		return khipu.Penalty(linebreak.InfinityDemerits)
 	}
-	penalty := cursor.Knot().(khipu.Penalty)
+	penalty := lb.knot.(khipu.Penalty)
 	ignore := false // final penalty found, ignore all other penalties
-	knot, ok := cursor.Peek()
+	knot, ok := lb.peek()
 	for ok {
 		if knot.Type() == khipu.KTPenalty {
-			cursor.Next() // advance to next penalty
+			lb.next() // move over knot
 			if ignore {
-				break // just skip over adjacent penalties
+				break
+			} else {
+				p := knot.(khipu.Penalty)
+				if p.Demerits() <= linebreak.InfinityMerits { // -10000 must break (like in TeX)
+					penalty = p
+					ignore = true
+				} else if p.Demerits() > penalty.Demerits() {
+					penalty = p
+				}
 			}
-			p := knot.(khipu.Penalty)
-			if p.Demerits() <= linebreak.InfinityMerits { // -10000 must break (like in TeX)
-				penalty = p
-				ignore = true
-			} else if p.Demerits() > penalty.Demerits() {
-				penalty = p
-			}
-			knot, ok = cursor.Peek() // now check next knot
+			knot, ok = lb.peek()
 		} else {
 			ok = false
 		}
 	}
 	p := khipu.Penalty(linebreak.CapDemerits(penalty.Demerits()))
-	return p, cursor.Mark()
+	return p
 }
+
+type segment struct {
+	length       linebreak.WSS //
+	breakDiscard linebreak.WSS // sum of discardable space while lookinf for next breakpoint
+}
+
+// append the width information of a knot at the end of a segment.
+// if the knot is a discardable item, s.breakDiscard is updated as well.
+func (s *segment) append(knot khipu.Knot) {
+	s.length = s.length.Add(linebreak.WSS{}.SetFromKnot(knot))
+	if knot.IsDiscardable() {
+		s.breakDiscard = s.breakDiscard.Add(linebreak.WSS{}.SetFromKnot(knot))
+	} else {
+		s.breakDiscard = linebreak.WSS{}
+	}
+}
+
+// width returns the widths of the current partial line, subtracting
+// space at the end of the segment (as this will be dropped).
+// width is respecting params.LeftSkip & RightSkip.
+func (s *segment) width(params *linebreak.Parameters) linebreak.WSS {
+	return s.length
+	// segw := s.length.Subtract(s.breakDiscard)
+	// w := linebreak.WSS{}.SetFromKnot(params.LeftSkip)
+	// segw = segw.Add(w)
+	// w = linebreak.WSS{}.SetFromKnot(params.RightSkip)
+	// segw = segw.Add(w)
+	// return segw
+}
+
+func (s *segment) reset(wss linebreak.WSS) {
+	s.length = wss
+	s.breakDiscard = linebreak.WSS{}
+}
+
+func (s *segment) String() string {
+	return fmt.Sprintf("|---%.2f---->", s.length.W.Points())
+}
+
+func (lb *linebreaker) linebreak(breakpoints []khipu.Mark) []khipu.Mark {
+	//panic("LINEBREAK")
+	lb.linecount++
+	gtrace.CoreTracer.Debugf("new line #%d", lb.linecount)
+	// reset pos, check, buffer
+	breakpoints = append(breakpoints, lb.mark())
+	lb.buffer = lb.buffer[:0]
+	lb.check = -1
+	lb.pos = -1
+	return breakpoints
+}
+
+func (lb *linebreaker) next() khipu.Knot {
+	if lb.pos < 0 {
+		if lb.cursor.Next() {
+			lb.knot = lb.cursor.Knot()
+			return lb.knot
+		}
+		return nil // end of khipu
+	}
+	lb.pos++
+	if lb.pos >= len(lb.buffer) {
+		if lb.cursor.Next() {
+			lb.buffer = append(lb.buffer, lb.cursor.Mark())
+		} else {
+			lb.eof = true
+			return nil
+		}
+	}
+	lb.knot = lb.buffer[lb.pos].Knot()
+	return lb.knot
+}
+
+func (lb *linebreaker) mark() khipu.Mark {
+	if lb.pos < 0 {
+		return lb.cursor.Mark()
+	}
+	return lb.buffer[lb.pos]
+}
+
+func (lb *linebreaker) peek() (khipu.Knot, bool) {
+	if lb.pos < 0 {
+		return lb.cursor.Peek()
+	}
+	if lb.pos+1 >= len(lb.buffer) {
+		return lb.cursor.Peek()
+	}
+	return lb.buffer[lb.pos+1].Knot(), true
+}
+
+func (lb *linebreaker) checkpoint() bool {
+	if lb.pos > -1 {
+		lb.buffer = lb.buffer[lb.pos:]
+		lb.check, lb.pos = 0, 0
+		lb.knot = lb.buffer[lb.pos].Knot()
+		return true
+	} else if lb.cursor.Knot() != nil {
+		lb.buffer = lb.buffer[:0]
+		lb.buffer = append(lb.buffer, lb.cursor.Mark())
+		lb.check, lb.pos = 0, 0
+		lb.knot = lb.buffer[lb.pos].Knot()
+		return true
+	}
+	return false
+}
+
+func (lb *linebreaker) backtrack() khipu.Knot {
+	if lb.check < 0 {
+		return nil
+	}
+	lb.pos = lb.check
+	lb.knot = lb.buffer[lb.pos].Knot()
+	return lb.knot
+}
+
+type provisionalMark int // provisional mark from an integer position
+
+func (m provisionalMark) Position() int    { return int(m) }
+func (m provisionalMark) Knot() khipu.Knot { return khipu.Penalty(-10000) }
--- engine/khipu/linebreak/firstfit/firstfit_test.go
+++ engine/khipu/linebreak/firstfit/firstfit_test.go
@@ -1,12 +1,16 @@
 package firstfit
 
 import (
+	"bytes"
 	"io"
 	"io/ioutil"
 	"strings"
 	"testing"
 
+	"github.com/npillmayer/gotype/core/config/tracing"
+
 	"github.com/npillmayer/gotype/core/config/gtrace"
+	"github.com/npillmayer/gotype/core/config/tracing/gologadapter"
 	"github.com/npillmayer/gotype/core/config/tracing/gotestingadapter"
 	"github.com/npillmayer/gotype/core/dimen"
 	"github.com/npillmayer/gotype/core/parameters"
@@ -21,10 +25,10 @@ func TestBuffer(t *testing.T) {
 	gtrace.CoreTracer = gotestingadapter.New()
 	teardown := gotestingadapter.RedirectTracing(t)
 	defer teardown()
-	lb := newTestLinebreaker(t, "Hello World!")
+	lb, _ := newTestLinebreaker(t, "Hello World!", 20)
 	k, ok := lb.peek()
 	if !ok || k.Type() != khipu.KTTextBox {
-		t.Logf("lb.pos=%d, lb.mark=%d", lb.pos, lb.mark)
+		t.Logf("lb.pos=%d, lb.mark=%d", lb.pos, lb.check)
 		t.Errorf("expected the first knot to be TextBox('Hello'), is %v", k)
 	}
 	if knot := lb.next(); k != knot {
@@ -37,7 +41,7 @@ func TestBacktrack(t *testing.T) {
 	gtrace.CoreTracer = gotestingadapter.New()
 	teardown := gotestingadapter.RedirectTracing(t)
 	defer teardown()
-	lb := newTestLinebreaker(t, "the quick brown fox jumps over the lazy dog.")
+	lb, _ := newTestLinebreaker(t, "the quick brown fox jumps over the lazy dog.", 30)
 	k := lb.next()
 	lb.checkpoint()
 	lb.next()
@@ -54,18 +58,66 @@ func TestBacktrack(t *testing.T) {
 		t.Errorf("remembered knot is %v, backtracked knot is %v", k, knot)
 	}
 }
+func TestLinebreak(t *testing.T) {
+	gtrace.CoreTracer = gologadapter.New()
+	// gtrace.CoreTracer = gotestingadapter.New()
+	// teardown := gotestingadapter.RedirectTracing(t)
+	// defer teardown()
+	lb, kh := newTestLinebreaker(t, "the quick brown fox jumps over the lazy dog.", 30)
+	breakpoints, err := lb.FindBreakpoints()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	t.Logf("# Paragraph with %d lines: %v", len(breakpoints)-1, breakpoints)
+	t.Logf("    |---------+---------+---------+---------+-----|")
+	j := 0
+	for i := 1; i < len(breakpoints); i++ {
+		//	t.Logf("%3d: %s", i, kh.Text(j, breakpoints[i].Position()))
+		text := kh.Text(j, breakpoints[i].Position())
+		t.Logf("%3d: %-45s|", i, justify(text, 30, i%2 == 0))
+		j = breakpoints[i].Position()
+	}
+	t.Fail()
+}
 
-func newTestLinebreaker(t *testing.T, text string) *linebreaker {
-	_, cursor, _ := setupFFTest(t, text, false)
-	parshape := linebreak.RectangularParShape(10 * 10 * dimen.BP)
+var princess = `In olden times when wishing still helped one, there lived a king whose daughters were all beautiful;`
+
+//var princess = `In olden times when wishing still helped one, there lived a king whose daughters were all beautiful; and the youngest was so beautiful that the sun itself, which has seen so much, was astonished whenever it shone in her face. Close by the king's castle lay a great dark forest, and under an old lime-tree in the forest was a well, and when the day was very warm, the king's child went out into the forest and sat down by the side of the cool fountain; and when she was bored she took a golden ball, and threw it up on high and caught it; and this ball was her favorite plaything.`
+
+func TestPrincess(t *testing.T) {
+	gtrace.CoreTracer = gologadapter.New()
+	// gtrace.CoreTracer = gotestingadapter.New()
+	// teardown := gotestingadapter.RedirectTracing(t)
+	// defer teardown()
+	lb, kh := newTestLinebreaker(t, princess, 45)
+	breakpoints, err := lb.FindBreakpoints()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	t.Logf("# Paragraph with %d lines: %v", len(breakpoints)-1, breakpoints)
+	t.Logf("     |---------+---------+---------+---------+-----|")
+	j := 0
+	for i := 1; i < len(breakpoints); i++ {
+		//	t.Logf("%3d: %s", i, kh.Text(j, breakpoints[i].Position()))
+		text := kh.Text(j, breakpoints[i].Position())
+		t.Logf("%3d: %-45s|", i, justify(text, 45, i%2 == 0))
+		j = breakpoints[i].Position()
+	}
+	t.Fail()
+}
+
+func newTestLinebreaker(t *testing.T, text string, len int) (*linebreaker, *khipu.Khipu) {
+	kh, cursor, _ := setupFFTest(t, text, false)
+	parshape := linebreak.RectangularParShape(dimen.Dimen(len) * 10 * dimen.BP)
 	lb, err := newLinebreaker(cursor, parshape, nil)
 	if err != nil {
 		t.Error(err)
 	}
-	return lb
+	return lb, kh
 }
 
 func setupFFTest(t *testing.T, paragraph string, hyphens bool) (*khipu.Khipu, linebreak.Cursor, io.Writer) {
+	gtrace.CoreTracer.SetTraceLevel(tracing.LevelError)
 	regs := parameters.NewTypesettingRegisters()
 	if hyphens {
 		regs.Push(parameters.P_MINHYPHENLENGTH, 3) // allow hyphenation
@@ -77,7 +129,6 @@ func setupFFTest(t *testing.T, paragraph string, hyphens bool) (*khipu.Khipu, li
 		t.Errorf("no Khipu to test; input is %s", paragraph)
 	}
 	//kh.AppendKnot(khipu.Penalty(linebreak.InfinityMerits))
-	gtrace.CoreTracer.Infof("input khipu=%s", kh.String())
 	cursor := linebreak.NewFixedWidthCursor(khipu.NewCursor(kh), 10*dimen.BP, 0)
 	var dotfile io.Writer
 	var err error
@@ -87,5 +138,60 @@ func setupFFTest(t *testing.T, paragraph string, hyphens bool) (*khipu.Khipu, li
 			t.Errorf(err.Error())
 		}
 	}
+	gtrace.CoreTracer.SetTraceLevel(tracing.LevelDebug)
 	return kh, cursor, dotfile
+}
+
+// -------------------------------------------------------------
+
+// crude implementation just for testing
+func justify(text string, l int, even bool) string {
+	t := strings.Trim(text, " \t\n")
+	d := l - len(t)
+	if d == 0 {
+		return t // fit
+	} else if d < 0 { // overfull box
+		return text + "\u25ae"
+	}
+	s := strings.Fields(text)
+	if len(s) == 1 {
+		return text
+	}
+	var b bytes.Buffer
+	W := 0 // length of all words
+	for _, w := range s {
+		W += len(w)
+	}
+	d = l - W // amount of WS to distribute
+	ws := d / (len(s) - 1)
+	r := d - ws*(len(s)-1) + 1
+	b.WriteString(s[0])
+	if even {
+		for j := 1; j < r; j++ {
+			for i := 0; i < ws+1; i++ {
+				b.WriteString(" ")
+			}
+			b.WriteString(s[j])
+		}
+		for j := r; j < len(s); j++ {
+			for i := 0; i < ws; i++ {
+				b.WriteString(" ")
+			}
+			b.WriteString(s[j])
+		}
+	} else {
+		for j := 1; j <= len(s)-r; j++ {
+			for i := 0; i < ws; i++ {
+				b.WriteString(" ")
+			}
+			b.WriteString(s[j])
+		}
+		for j := len(s) - r + 1; j < len(s); j++ {
+			for i := 0; i < ws+1; i++ {
+				b.WriteString(" ")
+			}
+			b.WriteString(s[j])
+		}
+	}
+	return b.String()
 }
