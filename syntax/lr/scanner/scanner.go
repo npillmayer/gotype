3f0c09d400339f53fd42026a84f9d9dbead62d13
Norbert Pillmayer <norbert@pillmayer.com>
2020-02-03 23:43:29 +0100

    Introduces tokenizer options

--- syntax/lr/scanner/scanner.go
+++ syntax/lr/scanner/scanner.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"text/scanner"
+
+	"github.com/npillmayer/gotype/core/config/gtrace"
 )
 
 // AnyToken is a helper flag: expect any token from the scanner.
@@ -31,14 +33,18 @@ type Tokenizer interface {
 // Create one with GoTokenizer.
 type DefaultTokenizer struct {
 	scanner.Scanner
-	lastToken rune
+	lastToken    rune
+	unifyStrings bool
 }
 
 // GoTokenizer creates a scanner/tokenizer accepting tokens similar to the Go language.
-func GoTokenizer(sourceID string, input io.Reader) *DefaultTokenizer {
+func GoTokenizer(sourceID string, input io.Reader, opts ...Option) *DefaultTokenizer {
 	t := &DefaultTokenizer{}
 	t.Init(input)
 	t.Filename = sourceID
+	for _, opt := range opts {
+		opt(t)
+	}
 	return t
 }
 
@@ -46,6 +52,11 @@ func GoTokenizer(sourceID string, input io.Reader) *DefaultTokenizer {
 func (t *DefaultTokenizer) NextToken(exp []int) (int, interface{}, uint64, uint64) {
 	t.lastToken = t.Scan()
 	if t.lastToken == scanner.EOF {
+		gtrace.SyntaxTracer.Debugf("DefaultTokenizer reached end of input")
+	}
+	if t.unifyStrings &&
+		(t.lastToken == scanner.RawString || t.lastToken == scanner.Char) {
+		t.lastToken = scanner.String
 	}
 	return int(t.lastToken), t.TokenText(), uint64(t.Position.Offset),
 		uint64(t.Pos().Offset - t.Position.Offset)
@@ -61,4 +72,37 @@ func Lexeme(token interface{}) string {
 	default:
 		return fmt.Sprintf("%v", t)
 	}
+}
+
+// Option configures a default tokenier.
+type Option func(p *DefaultTokenizer)
+
+const (
+	optionSkipComments uint = 1 << 1 // do not pass comments
+	optionUnifyStrings uint = 1 << 2 // treat raw strings and single chars as strings
+)
+
+// SkipComments set or clears mode-flag SkipComments.
+func SkipComments(b bool) Option {
+	return func(t *DefaultTokenizer) {
+		if !t.hasmode(optionSkipComments) && b ||
+			t.hasmode(optionSkipComments) && !b {
+			t.Mode |= scanner.SkipComments
+		}
+	}
+}
+
+// UnifyStrings sets or clears option UnifyStrings:
+// treat raw strings and single chars as strings.
+func UnifyStrings(b bool) Option {
+	return func(t *DefaultTokenizer) {
+		t.unifyStrings = b
+	}
+}
+
+func (t *DefaultTokenizer) hasmode(m uint) bool {
+	if m == optionUnifyStrings {
+		return t.unifyStrings
+	}
+	return t.Mode&m > 0
 }
