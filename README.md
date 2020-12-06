76e8d9abe5aa7ab2f4a36c02222290e7f6f31098
Norbert Pillmayer <norbert@pillmayer.com>
2020-12-06 13:41:39 +0100

    Gutting due to modularization

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# gotype
-some experiments on type in frames, still early draft
+## Some experiments on type in frames, still early draft
+
+Due to modularization this repo will be just a more or less empty hull soon.
--- backend/gfx/canvas.go
+++ backend/gfx/canvas.go
@@ -12,7 +12,7 @@ Picture, which holds a Canvas plus some administrative information.
 
 BSD License
 
-Copyright (c) 2017, Norbert Pillmayer <norbert@pillmayer.com>
+Copyright (c) 2017-21, Norbert Pillmayer <norbert@pillmayer.com>
 
 All rights reserved.
 
@@ -50,19 +50,18 @@ import (
 	"image/color"
 	"math/cmplx"
 
-	arithm "github.com/npillmayer/gotype/core/arithmetic"
-	"github.com/npillmayer/gotype/core/config/tracing"
-	"github.com/npillmayer/gotype/core/path"
-	"github.com/npillmayer/gotype/core/polygon"
-	dec "github.com/shopspring/decimal"
+	"github.com/npillmayer/arithm"
+	"github.com/npillmayer/arithm/path"
+	"github.com/npillmayer/arithm/polygon"
+	"github.com/npillmayer/schuko/tracing"
 )
 
-// We are tracing to the graphics tracer.
+// G traces to the graphics tracer.
 func G() tracing.Trace {
 	return tracing.GraphicsTracer
 }
 
-// The interface type for a drawing canvas
+// Canvas is an interface type for a drawing canvas.
 type Canvas interface {
 	W() float64                                                    // width
 	H() float64                                                    // height
@@ -70,7 +69,7 @@ type Canvas interface {
 	SetOption(int)                                                 // set a drawing option
 }
 
-// This is the type for the backend to work with
+// Picture is the type for the backend to work with.
 type Picture struct {
 	Name         string
 	canvas       Canvas
@@ -79,32 +78,33 @@ type Picture struct {
 	output       OutputRoutine
 }
 
-// A drawing pen
+// Pen is a drawing pen.
 type Pen struct {
-	diameter dec.Decimal
+	diameter float64
 	style    int
 }
 
-// A pen with a round nib
+// PENCIRCLE is a pen with a round nib
 const PENCIRCLE int = 1
 
-// A pen with a square nib
+// PENSQUARE is a pen with a square nib
 const PENSQUARE int = 2
 
-// Create a new Picture. Caller has to provide an identifier, a width,
+// NewPicture creates a
+// new Picture. Caller has to provide an identifier, a width,
 // a height and a Canvas.
 func NewPicture(name string, canvas Canvas) *Picture {
 	pic := &Picture{
 		Name:         name,
 		canvas:       canvas,
 		currentColor: color.Black,
-		currentPen:   NewPencircle(arithm.ConstOne),
+		currentPen:   NewPencircle(1.0),
 	}
 	return pic
 }
 
-// Create a new round pen.
-func NewPencircle(diam dec.Decimal) *Pen {
+// NewPencircle creates a new round pen.
+func NewPencircle(diam float64) *Pen {
 	pen := &Pen{
 		diameter: diam,
 		style:    PENCIRCLE,
@@ -112,8 +112,8 @@ func NewPencircle(diam dec.Decimal) *Pen {
 	return pen
 }
 
-// Create a new square pen.
-func NewPensquare(diam dec.Decimal) *Pen {
+// NewPensquare create a new square pen.
+func NewPensquare(diam float64) *Pen {
 	pen := &Pen{
 		diameter: diam,
 		style:    PENSQUARE,
@@ -134,6 +134,7 @@ func (pic *Picture) Fill(contour DrawableContour) {
 	pic.canvas.AddContour(contour, pendiam, nil, pic.currentColor)
 }
 
+// FillDraw is
 // MetaPost's filldraw command. Uses the current pen and color.
 func (pic *Picture) FillDraw(contour DrawableContour) {
 	pendiam, _ := pic.currentPen.diameter.Float64()
@@ -141,18 +142,21 @@ func (pic *Picture) FillDraw(contour DrawableContour) {
 	pic.canvas.AddContour(contour, pendiam, pic.currentColor, pic.currentColor)
 }
 
-// Set the current pen. Will be used for subsequent drawing operations.
+// SetPen sets
+// the current pen. Will be used for subsequent drawing operations.
 func (pic *Picture) SetPen(pen *Pen) {
 	pic.currentPen = pen
 }
 
-// Set the current color. Will be used for subsequent drawing operations.
+// SetColor sets
+// the current color. Will be used for subsequent drawing operations.
 func (pic *Picture) SetColor(color color.Color) {
 	pic.currentColor = color
 }
 
 // === Drawable Contour ======================================================
 
+// DrawableContour is an interface for a stroke.
 // The central operation on Canvases is AddContour, the drawing and/or filling
 // of a path. Different clients may have a different understanding of what a
 // path is and how to store it. Interface DrawableContour tries to be a common
@@ -167,7 +171,7 @@ type DrawableContour interface {
 	ToNextKnot() (k arithm.Pair, c1 arithm.Pair, c2 arithm.Pair)
 }
 
-// An immutable adapter to contours from cubic splines.
+// NewDrawablePath creates an immutable adapter to contours from cubic splines.
 func NewDrawablePath(path path.HobbyPath, controls path.SplineControls) DrawableContour {
 	pdrw := &pathdrawer{p: path, c: controls}
 	if path.IsCycle() {
@@ -193,9 +197,9 @@ func (pdrw *pathdrawer) IsCycle() bool {
 
 // implement interface DrawableContour
 func (pdrw *pathdrawer) Start() arithm.Pair {
-	G().Debugf("path start at %s", arithm.C2Pr(pdrw.p.Z(0)))
+	G().Debugf("path start at %s", arithm.Pair(pdrw.p.Z(0)))
 	pdrw.current = 0
-	return arithm.C2Pr(pdrw.p.Z(0))
+	return pdrw.p.Z(0)
 }
 
 // implement interface DrawableContour
@@ -207,15 +211,15 @@ func (pdrw *pathdrawer) ToNextKnot() (arithm.Pair, arithm.Pair, arithm.Pair) {
 	}
 	c1, c2 := pdrw.c.PostControl(pdrw.current-1), pdrw.c.PreControl(pdrw.current%(pdrw.p.N()))
 	if pdrw.current < pdrw.n && !cmplx.IsNaN(c1) {
-		G().Debugf("path next  at %s", arithm.C2Pr(pdrw.p.Z(pdrw.current)))
-		G().Debugf("     controls %s and %s", arithm.C2Pr(c1), arithm.C2Pr(c2))
+		G().Debugf("path next  at %s", arithm.Pair(pdrw.p.Z(pdrw.current)))
+		G().Debugf("     controls %s and %s", arithm.Pair(c1), arithm.Pair(c2))
 	} else {
-		G().Debugf("path next  at %s", arithm.C2Pr(pdrw.p.Z(pdrw.current)))
+		G().Debugf("path next  at %s", arithm.Pair(pdrw.p.Z(pdrw.current)))
 	}
-	return arithm.C2Pr(pdrw.p.Z(pdrw.current)), arithm.C2Pr(c1), arithm.C2Pr(c2)
+	return pdrw.p.Z(pdrw.current), c1, c2
 }
 
-// An immutable bridge to contours from polygons.
+// NewDrawablePolygon creates an immutable bridge to contours from polygons.
 func NewDrawablePolygon(pg polygon.Polygon) DrawableContour {
 	panic("not yet implemented: Contour for Polygon")
 	return nil
@@ -223,7 +227,8 @@ func NewDrawablePolygon(pg polygon.Polygon) DrawableContour {
 
 // === Output Routine ========================================================
 
-// Interface for output routines to ship out completed images
+// OutputRoutine is an interface for output routines to ship out
+// completed images.
 type OutputRoutine interface {
 	Shipout(pic *Picture, gfxFormat string) bool
 }
